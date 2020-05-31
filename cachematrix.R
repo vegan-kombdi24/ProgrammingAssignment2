35149fd1bd9d685beaef7f6791c261786726139e
vegan-kombdi24 <leronp2410@outlook.com>
2020-05-31 07:23:45 +0530

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,43 @@
 ## functions do
 
 ## Write a short comment describing this function
+## the valuve of x should be predefined matrix mat<-makeCacheMatrix(t"t is a predefined")
+##   or should be added like this bmat<-makeCacheMatrix(t<-matrix(1:2,2,2))
 
 makeCacheMatrix <- function(x = matrix()) {
+         
+         ma<- NULL
+         set<- function(y=matrix()){
+           x<<-y
+           ma <<- NULL
+         }
+      get<- function() x
+      setinv<- function(inverse) ma<<- inverse
+      getinv<- function() ma
+      
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
+## made use of matlib package 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ma <- x$getinv()
+  if(!is.null(ma)) {
+    message("getting cached data")
+    return(ma)
+  }
+  data <- x$get()
+  
+  ma <- Inverse(data)
+  x$setinv(ma)
+  ma
+  
+  
+  
 }
